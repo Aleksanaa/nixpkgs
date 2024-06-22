e202da971ff28329581dd75aed699bb5e19b4669
aleksana <me@aleksana.moe>
2024-06-22 23:54:14 +0800

    gtkdialog: 0.8.3 -> 0.8.5d

--- pkgs/by-name/gt/gtkdialog/package.nix
+++ pkgs/by-name/gt/gtkdialog/package.nix
@@ -1,38 +1,49 @@
 {
   lib,
   stdenv,
-  fetchurl,
-  fetchpatch,
-  gtk2,
+  fetchFromGitHub,
+  meson,
+  ninja,
   pkg-config,
+  bison,
+  flex,
+  wrapGAppsHook3,
+  vte,
+  gtk-layer-shell,
 }:
 
 stdenv.mkDerivation rec {
   pname = "gtkdialog";
-  version = "0.8.3";
+  version = "0.8.5d";
 
-  src = fetchurl {
-    url = "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/${pname}/${pname}-${version}.tar.gz";
-    sha256 = "ff89d2d7f1e6488e5df5f895716ac1d4198c2467a2a5dc1f51ab408a2faec38e";
+  src = fetchFromGitHub {
+    owner = "puppylinux-woof-CE";
+    repo = "gtkdialog";
+    rev = version;
+    hash = "sha256-Mox6P+mZnq7tEpOBYh+yKkTpP7nDHaLaPTzubauvnt0=";
   };
-  patches = [
-    # Pull Gentoo patch for -fno-common toolchain fix.
-    (fetchpatch {
-      name = "fno-common.patch";
-      url = "https://gitweb.gentoo.org/repo/gentoo.git/plain/x11-misc/gtkdialog/files/gtkdialog-0.8.3-fno-common.patch?id=98692e4c4ad494b88c4902ca1ab3e6541190bbe8";
-      sha256 = "1mh01krzpfy7lbbqx3xm71xsiqqgg67w4snv794wspfqkk2hicvz";
-    })
+
+  nativeBuildInputs = [
+    meson
+    ninja
+    pkg-config
+    bison
+    flex
+    wrapGAppsHook3
   ];
 
-  nativeBuildInputs = [ pkg-config ];
-  buildInputs = [ gtk2 ];
+  buildInputs = [
+    vte
+    gtk-layer-shell
+  ];
 
   meta = {
-    homepage = "https://code.google.com/archive/p/gtkdialog/";
-    # community links: http://murga-linux.com/puppy/viewtopic.php?t=111923 -> https://github.com/01micko/gtkdialog
+    homepage = "https://github.com/puppylinux-woof-CE/gtkdialog";
+    # community links: http://murga-linux.com/puppy/viewtopic.php?t=111923
     description = "Small utility for fast and easy GUI building from many scripted and compiled languages";
     mainProgram = "gtkdialog";
     license = lib.licenses.gpl2Plus;
+    maintainers = with lib.maintainers; [ aleksana ];
     platforms = lib.platforms.linux;
   };
 }
